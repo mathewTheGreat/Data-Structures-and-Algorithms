59d0f390cec6535bc8af375b0ec298f3f1aa93be
mathewTheGreat <44224652+mathewTheGreat@users.noreply.github.com>
2022-09-21 18:24:22 +0300

    Add files via upload

---
+++ HashMap.js
@@ -0,0 +1,53 @@
+class HashMap {
+    constructor(initialCapacity = 10) {
+    this.buckets = new Array(initialCapacity);
+    this.collisions = 0;
+    }
+    
+    set(key, value) {
+    const bucketIndex = this.getIndex(key);
+    if(this.buckets[bucketIndex]) {
+    this.buckets[bucketIndex].push({key, value});
+    if(this.buckets[bucketIndex].length > 1) { this.collisions++; }
+    } else {
+    this.buckets[bucketIndex] = [{key, value}];
+    }
+    return this;
+    }
+    
+    get(key) {
+    const bucketIndex = this.getIndex(key);
+    for (let arrayIndex = 0; arrayIndex < this.buckets[bucketIndex].length; arrayIndex++) {
+    const entry = this.buckets[bucketIndex][arrayIndex];
+    if(entry.key === key) {
+    return entry.value
+    }
+    }
+    }
+    
+    hash(key) {
+    let hashValue = 0;
+    const stringTypeKey = `${key}${typeof key}`;
+    
+    for (let index = 0; index < stringTypeKey.length; index++) {
+    const charCode = stringTypeKey.charCodeAt(index);
+    hashValue += charCode << (index * 8);
+    }
+    
+    return hashValue;
+    }
+    
+    getIndex(key) {
+    const indexHash = this.hash(key);
+    const index = indexHash % this.buckets.length;
+    return index;
+    }
+    }
+
+    const hashMap = new HashMap();
+
+    hashMap.set('cars', 4);
+    hashMap.set('cans', 3);
+    hashMap.set('caps', 2);
+    hashMap.set('cats', 6);
+    console.log(hashMap)
