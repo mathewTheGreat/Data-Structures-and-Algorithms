53581382f7f0215d8b6664c34a6d70a2270eb744
mathewTheGreat <44224652+mathewTheGreat@users.noreply.github.com>
2022-10-05 11:45:38 +0300

    Union Find
    Data structure that keeps track of a set of elements partitioned into a number of disjoint (non-overlapping) subsets.

--- Kruskal's_Algorithm.js
+++ Kruskal's_Algorithm.js
@@ -6,3 +6,48 @@
 
 3 The algorithm terminates when every edge has been processed
 */
+
+class UnionFind {
+
+  constructor(elements) {
+
+    this.size = elements.length
+
+    this.parent = {}
+
+    elements.forEach(e => (this.parent[e] = e))
+
+  }
+
+  union(a, b) {
+
+    let rootA = this.find(a)
+    let rootB = this.find(b)
+
+    if(rootA === rootB) return;
+
+    if(rootA < rootB) {
+      if(this.parent[b] != b) this.union(this.parent[b], a)
+      this.parent[a] = this.parent[a]
+    }else {
+      if(this.parent[a] != a) this.union(this.parent[a], b)
+      this.parent[b] = this.parent[a]
+    }
+
+  }
+
+  find(a) {
+  
+    while(this.parent[a] !== a) {
+      a = this.parent[a]
+    }
+
+    return a
+
+  }
+
+  connected(a, b) {
+    return this.find(a) === this.find(b)
+  }
+
+}
